034b64877d833e4e1c7e7c22784b6fecbc4c50e7
GerelPR <117137519+GerelPR@users.noreply.github.com>
2024-12-08 14:55:54 +0800

    g

---
+++ lab-management-system/src/main/java/com/labmanagement/Main.java
@@ -0,0 +1,18 @@
+package com.labmanagement;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+/**
+ * Hello world!
+ */
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Lab Management System");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
